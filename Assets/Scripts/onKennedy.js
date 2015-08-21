8ff9bc3c6e797a1e38aebfe00a7863d77070dbe8
bdruiz <bdruizr@correo.udistrital.edu.co>
2015-08-21 13:04:45 -0500

    Agregando Escena Mapa General

---
+++ Assets/Scripts/onKennedy.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+if(Input.GetMouseButtonDown(0)){ //Click izq Mouse
+Invoke("cargar",1);
+
+	}
+
+}
+
+function cargar(){
+Application.LoadLevel("Terreno");
+}
